ff5b374e46dc8c94bb588254a3b64996aed9672a
caio <caio.yugo11@gmail.com>
2025-04-02 13:14:49 -0300

    chore: pointers

--- p1/pilha/pilha.c
+++ p1/pilha/pilha.c
@@ -1,29 +1,21 @@
 #include "pilha.h"
 
+int pop (t_pilha *p, int *removido){
+    if (pilha_vazia(p)) return 0; // da onde saiu pilha_vazia?
+    *removido = p->dados[--p->topo]; // pilha p aponta para os dados da struct pilha
+                                    // --p diminuindo os dados, depois aponta para o topo?
+    return 1;
+}
+
+int push(int i, t_pilha *p){
+    if (pilha_cheia(p)) return 0;
+    p -> dados[p->topo++] = i;
+    return 1;
+}
+
 int main (){
-    int chose = 1;
-    do {
-        printf("Escolha o exercicio!\n");
-        printf("Pilhas: 1-7 \n0-sair \nInput: ");
-        scanf("%d", &chose);
-        // melhor usar string?
-        // getc("");
-        //case "1"
-        switch (chose)
-        {
-            case 1:{
-                return 0;
-            }break;
-            
-            case 2:{
-
-            }break;
-            
-            case 0:{
-
-            }break;
-        }
-    } while (chose != 0);
-
+    int num;
+    printf("Insira os dados para a pilha: ");
+    scanf("%d",&num);
     return 0;
 }
