46c0ace29279be13a546eb8ec79aea1f965b367d
caio <caio.yugo11@gmail.com>
2025-04-29 11:25:12 -0300

    chore: p1-ex

--- p1/Exercicio-nota/main.c
+++ p1/Exercicio-nota/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> 
 
 typedef struct{
-    int placa;
+    char placa[8];
     int manobras;
 } Carro;
 
@@ -10,39 +11,39 @@ int main(){
     Carro estacionamento[10];
     int topo = 0; // Contador de carros 
     char operacao; // '0' para entrada, '1' para saída
-    int placa; // Placa do carro copiada da struct
-    printf("Placa do carro como int entao ha limite de memoria\n");
+    char placa[8]; // Placa do carro copiada da struct
+    printf("Placa do carro como string\n");
     while (1)  {
-        printf("Input para entrada ou saída (ex: 0 1234 ou 1 1234):\n");
-        if (scanf(" %c %d", &operacao, &placa) != 2) { // INPUT
+        printf("Input para entrada ou saída (ex: 0(entrada) ABC1234(placa) -> 1(saida) ABC1234(placa)):\n");
+        if (scanf(" %c %7s", &operacao, placa) != 2) { // INPUT
             printf("Entrada inválida. Encerrando programa.\n");
             break;
         }
         if (operacao == '0') { // ENTRADA -> cadastra a placa do carro
             if (topo >= 10) { // limite == 10 carros
-                printf("Carro %d chegou, mas estacionamento cheio. Indo embora.\n", placa);
+                printf("Carro %s chegou, mas estacionamento cheio. Indo embora.\n", placa);
             } else {
-                estacionamento[topo].placa = placa; // placa copiada da struct
+                strcpy(estacionamento[topo].placa, placa); // placa copiada da struct
                 estacionamento[topo].manobras = 0; // Manobras zeradas
                 topo++; // Adiciona o carro
-                printf("Carro %d entrou no estacionamento.\n", placa);
+                printf("Carro %s entrou no estacionamento.\n", placa);
             }
         } else if (operacao == '1') { // SAIDA
             int i, j;
             int encontrado = 0; 
             for (i = 0; i < topo; i++) { // Pesquisa a placa do carro e verifica 
-                if (estacionamento[i].placa == placa) {
+                if (strcmp(estacionamento[i].placa, placa) == 0) {
                     encontrado = 1;
                     break;
                 }
             }
             if (!encontrado) { // Placa do carro nao esta no array
-                printf("Carro %d não está no estacionamento.\n", placa);
+                printf("Carro %s não está no estacionamento.\n", placa);
             } else { // Carro encontrado, codigo complexo a seguir
                 for (j = i + 1; j < topo; j++) {
                     estacionamento[j].manobras++; // carro no array manobrado
                 }
-                printf("Carro %d saiu do estacionamento. Foi manobrado %d vezes.\n", estacionamento[i].placa, estacionamento[i].manobras); 
+                printf("Carro %s saiu do estacionamento. Foi manobrado %d vezes.\n", estacionamento[i].placa, estacionamento[i].manobras); 
                 // Reorganiza o estacionamento (remove o carro)
                 for (j = i; j < topo - 1; j++) {
                     estacionamento[j] = estacionamento[j + 1]; // move o carro para a esquerda
