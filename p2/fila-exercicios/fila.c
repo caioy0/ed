abc4fd7bebfb04d2b91454fd1bfd8bfea21c576e
caio <caio.yugo11@gmail.com>
2025-05-27 14:14:42 -0300

    chore: p2 segunda lista

---
+++ p2/fila-exercicios/fila.c
@@ -0,0 +1,100 @@
+#include "fila.h"
+
+t_fila *constroi_fila (int capacidade){
+    t_fila *fila = (t_fila *) malloc (sizeof(t_fila));
+    fila->capacidade = capacidade;
+    fila->primeiro = 0;
+    fila->ultimo = 0;
+    fila->ocupacao = 0;
+    fila->dados = (int *) malloc (capacidade * sizeof(int));
+   return fila; 
+}
+
+int fila_cheia (t_fila *fila){
+    return fila->ocupacao == fila->capacidade;
+}
+
+int fila_vazia (t_fila *fila){
+    return fila->ocupacao == 0;
+}
+
+void exibir_fila (t_fila *fila){
+    if (fila_vazia(fila)) printf("Fila vazia");
+    int i = fila->primeiro;
+    do {
+        printf("Fila: %d\n", fila->dados[i]);
+        i = proxima(i, fila->capacidade);
+    } while (i!=fila->ultimo);
+    printf("\n");
+}
+
+int enfileira (int i, t_fila *fila){
+    if (fila_cheia(fila)) return 0;
+    fila->dados[fila->ultimo] = i;
+    fila->ultimo = proxima(fila->ultimo, fila->capacidade);
+    fila->ocupacao++;
+    return 1;
+}
+
+int desenfileira (int *i, t_fila *fila){
+    if (fila_vazia(fila)) return 0;
+    *i = fila->dados[fila->primeiro];
+    fila->primeiro = proxima(fila->primeiro, fila->capacidade);
+    fila->ocupacao--;
+    return 1;
+}
+
+int proxima(int pos, int capacidade){
+    return (pos+1) % capacidade;
+}
+
+void mostra_vetor (t_fila *fila){
+    if (fila_vazia(fila)){
+        for (int i=0; i< fila->capacidade; i++){
+            printf("_ ");
+        }
+    } else if (fila_cheia(fila)){
+        for (int i=0; i<fila->capacidade; i++){
+            printf ("%d", fila->dados[i]);   
+        }
+    } else if (fila->primeiro < fila->ultimo){
+        for (int i=0; i<fila->primeiro; i++)
+            printf("_ ");
+        for (int i=fila->primeiro; i<fila->ultimo;i++)
+            printf("%d", fila->dados[i]);
+        for (int i=fila->ultimo; i<fila->capacidade; i++)
+            printf ("_ ");
+    } else{
+        for (int i=0; i<fila->ultimo; i++)
+            printf("%d", fila->dados[i]);
+        for (int i=fila->ultimo; i<fila->primeiro;i++)
+            printf("_ ");
+        for (int i=fila->primeiro; i<fila->capacidade; i++)
+            printf ("%d", fila->dados[i]);
+    }
+    printf("\n");
+}
+
+void mostra_fila (t_fila *fila){
+
+}
+
+void exibe_primeiro(t_fila *fila){
+    if (fila_vazia(pf)) printf("\n");
+    else printf("%d\n", pf-> primeiro->info);
+}
+
+int devolve_primeiro (t_fila *fila){
+    if (fila_vazia(pf)) return -1;
+    return pf->primeiro->info;
+}
+
+void exibe_ultimo (t_fila *fila){
+    if (fila_vazia(pf)) printf("\n");
+    else printf("%d\n", pf->ultimo->info);
+}
+
+int devolve_ultimo(t_fila *fila){
+    if (fila_vazia(pf)) return -1;
+    return pf->primeiro->info;
+}
---
+++ p2/fila-exercicios/fila.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int * dados;
+    int primeiro, ultimo;
+    int capacidade, ocupacao;
+} t_fila;
+
+t_fila * constroi_fila (int);
+int fila_cheia (t_fila *);
+int fila_vazia (t_fila *);
+void exibir_fila (t_fila *);
+int enfileira (int , t_fila *);
+int desenfileira (int *, t_fila *);
+int proxima (int pos, int capacidade);
+void mostra_vetor (t_fila *fila);
+void mostra_fila(t_fila *);
+void exibe_primeiro (t_fila *);
+int devolve_primeiro (t_fila *);
+void exibe_ultimo (t_fila *);
+int devolve_ultimo (t_fila *);
